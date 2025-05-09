1261c8381f04f8188c5ef5028c7709acea9e50dc
Baris Caliskan <clskn.baris22@gmail.com>
2025-05-09 17:28:53 +0300

    Add more interactive elements to the scene

--- TestScene.tsx
+++ TestScene.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import katex from "katex";
 import Board from "./Board";
 import Point from "./Point";
 import Line from "./Line";
@@ -9,6 +10,7 @@ export default function TestScene() {
   const [p1, setP1] = useState({ x: 1, y: 1 });
   const [p2, setP2] = useState({ x: 2, y: 2 });
   const [p3, setP3] = useState({ x: -1, y: 1 });
+  const [a, setA] = useState(0.5);
 
   const polynomial = (x: number) => {
     // The polynomial that passes through the three given points
@@ -19,36 +21,52 @@ export default function TestScene() {
       ((x - p1.x) * (x - p3.x)) / ((p2.x - p1.x) * (p2.x - p3.x));
     const l3 = (x: number) =>
       ((x - p1.x) * (x - p2.x)) / ((p3.x - p1.x) * (p3.x - p2.x));
-    return l1(x) * p1.y + l2(x) * p2.y + l3(x) * p3.y;
+    return l1(x) * p1.y + l2(x) * p2.y + l3(x) * p3.y + a;
   };
 
   return (
-    <Board
-      className="w-full h-[500px] border rounded-md bg-white"
-      options={{
-        unit: 50,
-      }}
-    >
-      <Grid
-        options={{
-          grid: {
-            visible: true,
-            stroke: "#ccc",
-            strokeWidth: 0.5,
-            gap: 1,
-          },
-          axes: {
-            visible: true,
-            stroke: "#000",
-            strokeWidth: 1,
-          },
-        }}
+    <div className="flex flex-col gap-4">
+      <input
+        type="range"
+        min={0}
+        max={10}
+        step={0.1}
+        value={a}
+        onChange={(e) => setA(+e.target.value)}
       />
-      <FunctionPlot f={polynomial} />
-      <Point x={0} y={0} />
-      <Point x={p1.x} y={p1.y} onDrag={(x, y) => setP1({ x, y })} />
-      <Point x={p2.x} y={p2.y} onDrag={(x, y) => setP2({ x, y })} />
-      <Point x={p3.x} y={p3.y} onDrag={(x, y) => setP3({ x, y })} />
-    </Board>
+      <div>
+        <span
+          dangerouslySetInnerHTML={{
+            __html: katex.renderToString("a = " + a),
+          }}
+        />
+      </div>
+      <Board
+        className="w-full h-[500px] border rounded-md bg-white"
+        options={{
+          unit: 50,
+        }}
+      >
+        <Grid
+          options={{
+            grid: {
+              visible: true,
+              stroke: "#ccc",
+              strokeWidth: 0.5,
+              gap: 1,
+            },
+            axes: {
+              visible: true,
+              stroke: "#000",
+              strokeWidth: 1,
+            },
+          }}
+        />
+        <FunctionPlot f={polynomial} />
+        <Point x={p1.x} y={p1.y} onDrag={(x, y) => setP1({ x, y })} />
+        <Point x={p2.x} y={p2.y} onDrag={(x, y) => setP2({ x, y })} />
+        <Point x={p3.x} y={p3.y} onDrag={(x, y) => setP3({ x, y })} />
+      </Board>
+    </div>
   );
 }
